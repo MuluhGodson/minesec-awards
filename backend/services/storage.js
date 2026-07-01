d82b6aee075ae3b2fa86566ea2dad985fe2aa4aa
PizieDust <playersrebirth@gmail.com>
2026-07-01 07:58:33 +0100

    construct service account

--- backend/services/storage.js
+++ backend/services/storage.js
@@ -2,7 +2,20 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getStorage } = require('firebase-admin/storage');
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE || 'service_account',
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universeDomain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
+
 // 1. Initialize Firebase using the modular functions
 initializeApp({
     credential: cert(serviceAccount),
